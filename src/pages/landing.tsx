88637227067078d3c1a307ef9ea2fc02215b56e4
ankan5415 <ankur.boyed@gmail.com>
2024-01-31 16:22:20 -0800

    Fix: deployment errors

--- src/pages/landing.tsx
+++
@@ -1,7 +0,0 @@
-import Landing from "@/features/webflow-landing";
-import { Inter } from "next/font/google";
-import Image from "next/image";
-
-export default function Page() {
-	return <Landing />;
-}
