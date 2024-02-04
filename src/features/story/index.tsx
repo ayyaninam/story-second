0f482b3002f142b630f6afa916546f66754b66f1
5war00p <maddisaiswaroop@gmail.com>
2024-02-04 19:02:12 +0530

    feat: background-blur

--- src/features/story/index.tsx
+++ src/features/story/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
 	ChevronLeft,
 	ChevronRight,
@@ -25,11 +26,14 @@ import Format from "@/utils/format";
 import StoryScreen from "./story-screen";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useEffect, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import { cn } from "@/utils";
 
 const MAX_SUMMARY_LENGTH = 250;
 
 export default function GeneratedStory() {
 	const router = useRouter();
+	const isDesktop = useMediaQuery("(min-width: 1280px)");
 	const [showFullDescription, setShowFullDescription] = useState(false);
 	const [enableQuery, setEnableQuery] = useState(true);
 
@@ -168,6 +172,15 @@ export default function GeneratedStory() {
 				<div className="relative rounded-lg border-[1px] w-full border-border bg-border bg-blend-luminosity px-2 lg:px-5 py-2">
 					<div className="flex flex-col md:flex-row items-center justify-center h-full">
 						<div className="w-full md:max-w-[1500px] border-[1px] rounded-bl-lg rounded-br-lg lg:rounded-br-lg lg:rounded-tr-lg lg:rounded-tl-sm lg:rounded-bl-sm flex flex-col lg:flex-row justify-stretch">
+							{/* eslint-disable-next-line @next/next/no-img-element */}
+							<img
+								alt="Background Blur"
+								// This url gonna change based on the current rendering frame
+								src="https://ik.imagekit.io/storybird/staging/images/849ce875-b59f-442d-b18f-548ff2bc7afc/1_823376133.webp"
+								className={
+									"absolute invisible xl:visible xl:w-full xl:h-min xl:max-w-screen-lg left-2 top-16 blur-lg"
+								}
+							/>
 							<div className="relative w-full lg:max-w-[80%] rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg aspect-video">
 								<StoryScreen />
 							</div>
