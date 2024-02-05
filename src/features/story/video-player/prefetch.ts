f47e6962c5d89bb47710b9455c386b96ecb973ac
Oscar <odrr.99@gmail.com>
2024-02-04 16:54:26 -0800

    fix: remotion flickering with premounting strategy

--- src/features/story/video-player/prefetch.ts
+++ src/features/story/video-player/prefetch.ts
@@ -1,59 +1,41 @@
 import pLimit from "p-limit";
 import { prefetch } from "remotion";
-import { preloadGif } from "@remotion/gif";
 import { RemotionSegment } from "./constants";
 
 const limit = pLimit(10);
 
 const prefetchSegmentAssets = async (segment: RemotionSegment) => {
-	try {
-		if (segment.type === "page") {
-			if (segment.visual.format === "video") {
-				await prefetch(segment.visual.videoURL, {
-					method: "base64",
-					contentType: "video/mp4",
-				}).waitUntilDone();
-			}
-			if (segment.visual.format === "image") {
-				await prefetch(segment.visual.imageURL, {
-					method: "base64",
-				}).waitUntilDone();
-			}
-			if (segment.visual.format === "gif") {
-				await preloadGif(segment.visual.gifURL).waitUntilDone();
-			}
-			if (segment.audioURL) {
-				await prefetch(segment.audioURL, {
-					contentType: "audio/mpeg",
-				}).waitUntilDone();
-			}
-		}
+  try {
+    if (segment.type === "page") {
+      if (segment.visual.format === "video") {
+        await prefetch(segment.visual.videoURL, {
+          method: "base64",
+          contentType: "video/mp4",
+        }).waitUntilDone();
+      }
+      if (segment.audioURL) {
+        await prefetch(segment.audioURL, {
+          contentType: "audio/mpeg",
+        }).waitUntilDone();
+      }
+    }
 
-		if (segment.type === "intermediate") {
-			if (segment.visual.format === "video") {
-				await prefetch(segment.visual.videoURL, {
-					method: "base64",
-					contentType: "video/mp4",
-				}).waitUntilDone();
-			}
-			if (segment.visual.format === "image") {
-				await prefetch(segment.visual.imageURL, {
-					contentType: "image/gif",
-					method: "base64",
-				}).waitUntilDone();
-			}
-			if (segment.visual.format === "gif") {
-				await preloadGif(segment.visual.gifURL).waitUntilDone();
-			}
-		}
-	} catch (e) {
-		console.error(e);
-	}
+    if (segment.type === "intermediate") {
+      if (segment.visual.format === "video") {
+        await prefetch(segment.visual.videoURL, {
+          method: "base64",
+          contentType: "video/mp4",
+        }).waitUntilDone();
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export const prefetchAssets = async (segments: RemotionSegment[]) => {
-	const prefetchAssetsPromises = segments.map((segment) =>
-		limit(() => prefetchSegmentAssets(segment))
-	);
-	await Promise.all(prefetchAssetsPromises);
+  const prefetchAssetsPromises = segments.map((segment) =>
+    limit(() => prefetchSegmentAssets(segment))
+  );
+  await Promise.all(prefetchAssetsPromises);
 };
