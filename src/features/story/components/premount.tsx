10f4acc806d35549f595bf9c2232fd6791434f64
ankan5415 <ankur.boyed@gmail.com>
2024-02-06 19:10:42 -0800

    preloading

--- src/features/story/components/premount.tsx
+++ src/features/story/components/premount.tsx
@@ -3,42 +3,42 @@ import React, { useContext, useMemo } from "react";
 import { Internals, TimelineContextValue, useVideoConfig } from "remotion";
 
 type PremountProps = {
-  for: number;
-  children: React.ReactNode;
+	for: number;
+	children: React.ReactNode;
 };
 
 export const Premount: React.FC<PremountProps> = ({
-  for: premountFor,
-  children,
+	for: premountFor,
+	children,
 }) => {
-  const { id } = useVideoConfig();
-  const sequenceContext = useContext(Internals.SequenceContext);
+	const { id } = useVideoConfig();
+	const sequenceContext = useContext(Internals.SequenceContext);
 
-  const context = useContext(Internals.Timeline.TimelineContext);
-  const value: TimelineContextValue = useMemo(() => {
-    const contextOffset = sequenceContext
-      ? sequenceContext.cumulatedFrom + sequenceContext.relativeFrom
-      : 0;
+	const context = useContext(Internals.Timeline.TimelineContext);
+	const value: TimelineContextValue = useMemo(() => {
+		const contextOffset = sequenceContext
+			? sequenceContext.cumulatedFrom + sequenceContext.relativeFrom
+			: 0;
 
-    const currentFrame = (context?.frame[id] ?? 0) - contextOffset;
-    const playing = currentFrame < premountFor ? false : context.playing;
-    const imperativePlayingValue =
-      currentFrame < premountFor ? false : context.imperativePlaying.current;
-    const frameValue = Math.max(0, currentFrame - premountFor) + contextOffset;
+		const currentFrame = (context?.frame[id] ?? 0) - contextOffset;
+		const playing = currentFrame < premountFor ? false : context.playing;
+		const imperativePlayingValue =
+			currentFrame < premountFor ? false : context.imperativePlaying.current;
+		const frameValue = Math.max(0, currentFrame - premountFor) + contextOffset;
 
-    return {
-      ...context,
-      playing,
-      imperativePlaying: {
-        current: imperativePlayingValue,
-      },
-      frame: { [id]: frameValue },
-    };
-  }, [context, premountFor, sequenceContext, id]);
+		return {
+			...context,
+			playing,
+			imperativePlaying: {
+				current: imperativePlayingValue,
+			},
+			frame: { [id]: frameValue },
+		};
+	}, [context, premountFor, sequenceContext, id]);
 
-  return (
-    <Internals.Timeline.TimelineContext.Provider value={value}>
-      {children}
-    </Internals.Timeline.TimelineContext.Provider>
-  );
+	return (
+		<Internals.Timeline.TimelineContext.Provider value={value}>
+			{children}
+		</Internals.Timeline.TimelineContext.Provider>
+	);
 };
