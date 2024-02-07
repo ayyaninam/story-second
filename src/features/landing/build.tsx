da42adb037639fb0c95846a7391b9b7298cb2efc
ankan5415 <ankur.boyed@gmail.com>
2024-02-06 23:30:59 -0800

    Fix: refactor enums in landing page

--- src/features/landing/build.tsx
+++ src/features/landing/build.tsx
@@ -12,8 +12,8 @@ import Routes from "@/routes";
 
 const queryClient = new QueryClient();
 
-const getEnumKeys = (Enum: any) => {
-	return Object.keys(Enum).filter((x) => !(parseInt(x) >= 0));
+const keys = (Enum: any) => {
+	return Object.keys(Enum).filter((key) => isNaN(Number(key)));
 };
 
 const App = () => {
@@ -45,6 +45,7 @@ const App = () => {
 	// Handlers
 	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+
 		const WebStory = await CreateWebstory.mutateAsync({
 			image_style: options.style,
 			language: options.language,
@@ -247,13 +248,14 @@ const App = () => {
 							onChange={(e) => {
 								setOptions((prev) => ({
 									...prev,
-									language: getEnumKeys(StoryLanguages).findIndex(
-										(el) => el === e.target.value
-									),
+									language:
+										StoryLanguages[
+											e.target.value as keyof typeof StoryLanguages
+										],
 								}));
 							}}
 						>
-							{getEnumKeys(StoryLanguages).map((label, index) => (
+							{keys(StoryLanguages).map((label, index) => (
 								<option key={index}>{label}</option>
 							))}
 						</select>
@@ -262,13 +264,12 @@ const App = () => {
 							onChange={(e) =>
 								setOptions((prev) => ({
 									...prev,
-									length: getEnumKeys(StoryLengths).findIndex(
-										(el) => el === e.target.value
-									),
+									length:
+										StoryLengths[e.target.value as keyof typeof StoryLengths],
 								}))
 							}
 						>
-							{getEnumKeys(StoryLengths).map((label, index) => (
+							{keys(StoryLengths).map((label, index) => (
 								<option key={index}>{label}</option>
 							))}
 						</select>
@@ -276,13 +277,14 @@ const App = () => {
 							onChange={(e) =>
 								setOptions((prev) => ({
 									...prev,
-									style: getEnumKeys(StoryImageStyles).findIndex(
-										(el) => el === e.target.value
-									),
+									style:
+										StoryImageStyles[
+											e.target.value as keyof typeof StoryImageStyles
+										],
 								}))
 							}
 						>
-							{getEnumKeys(StoryImageStyles).map((label, index) => (
+							{keys(StoryImageStyles).map((label, index) => (
 								<option key={index}>{label}</option>
 							))}
 						</select>
