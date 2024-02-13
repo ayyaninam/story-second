98b66ef9c8a0f1057b2731697884435d5636e800
5war00p <maddisaiswaroop@gmail.com>
2024-02-13 14:44:43 +0530

    fix: submit button hover states

--- src/features/landing/build.tsx
+++ src/features/landing/build.tsx
@@ -13,7 +13,6 @@ import {
 } from "@/utils/enums";
 import Routes from "@/routes";
 import FileUpload from "./components/file-upload";
-import toast, { Toaster } from "react-hot-toast";
 import { CreateInitialStoryQueryParams } from "@/types";
 import { ImageRatios } from "@/utils/image-ratio";
 
@@ -121,19 +120,19 @@ const App = () => {
 							>
 								<path
 									d="M0.5 8.30348C0.602416 12.4923 3.97192 15.8618 8.16074 15.9744C8.1505 11.7549 4.71955 8.32397 0.5 8.30348Z"
-									fill={isButtonDisabled ? "#1F323D" : "#FFF"}
+									fill={"#FFF"}
 								/>
 								<path
 									d="M16.4693 7.67606C16.3669 3.48724 12.9974 0.117736 8.80859 0.0153198C8.81884 4.23487 12.2498 7.66582 16.4693 7.67606Z"
-									fill={isButtonDisabled ? "#1F323D" : "#FFF"}
+									fill={"#FFF"}
 								/>
 								<path
 									d="M8.17051 0.00508118C3.98169 0.107497 0.612182 3.477 0.509766 7.66583C4.72931 7.65558 8.16027 4.22463 8.17051 0.00508118Z"
-									fill={isButtonDisabled ? "#1F323D" : "#FFF"}
+									fill={"#FFF"}
 								/>
 								<path
 									d="M8.80836 15.9949H8.61377H16.4998V8.30348C12.2598 8.30348 8.80836 11.7549 8.80836 15.9949Z"
-									fill={isButtonDisabled ? "#1F323D" : "#FFF"}
+									fill={"#FFF"}
 								/>
 							</svg>
 
@@ -303,11 +302,7 @@ const App = () => {
 							))}
 						</select>
 					</div>
-					<button
-						type="submit"
-						disabled={isButtonDisabled}
-						className="button hero-submit-button w-button button-nosubmit"
-					>
+					<button type="submit" disabled={isButtonDisabled}>
 						<svg
 							width="17"
 							height="16"
