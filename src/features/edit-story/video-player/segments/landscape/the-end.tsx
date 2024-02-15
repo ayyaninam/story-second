ea604afac4a5e7989eafb4f122c35eb71f45a90b
Oscar <odrr.99@gmail.com>
2024-02-15 12:12:07 -0800

    visual(remotion): add logo watermark the end for landscape and portrait formats

--- src/features/edit-story/video-player/segments/landscape/the-end.tsx
+++ src/features/edit-story/video-player/segments/landscape/the-end.tsx
@@ -1,64 +1,50 @@
 import React, { CSSProperties } from "react";
 import {
-	AbsoluteFill,
-	staticFile,
-	Img,
-	useCurrentFrame,
-	interpolate,
-	useVideoConfig,
+  AbsoluteFill,
+  staticFile,
+  Img,
+  useCurrentFrame,
+  interpolate,
 } from "remotion";
-import { VIDEO_FPS, PREMOUNT_FRAMES } from "../../constants";
+import { VIDEO_FPS } from "../../constants";
 
 const logoWatermarkContainer: CSSProperties = {
-	justifyContent: "center",
-	alignItems: "center",
+  justifyContent: "center",
+  alignItems: "center",
 };
+
 const logoWatermark: CSSProperties = {
-	width: 300,
+  width: 300,
+};
+
+const animatedContainer: CSSProperties = {
+  backgroundColor: "#1e2e75",
 };
 
 const TheEndSegment = () => {
-	const frame = useCurrentFrame();
-	const { durationInFrames } = useVideoConfig();
+  const frame = useCurrentFrame();
+  
+  const fadeInOpacity = interpolate(frame, [0, VIDEO_FPS], [0, 1], {
+    extrapolateLeft: "clamp",
+    extrapolateRight: "clamp",
+  });
+  
 
-	const fadeInOpacity = interpolate(frame, [0, VIDEO_FPS], [0, 1], {
-		extrapolateLeft: "clamp",
-		extrapolateRight: "clamp",
-	});
+  const animatedLogoWatermark: CSSProperties = {
+    ...logoWatermark,
+    opacity: fadeInOpacity,
+  };
 
-	const fadeOutOpacity = interpolate(
-		frame,
-		[
-			durationInFrames - PREMOUNT_FRAMES - VIDEO_FPS,
-			durationInFrames - PREMOUNT_FRAMES - 10, // a bit before ending video
-		],
-		[1, 0],
-		{
-			extrapolateLeft: "clamp",
-			extrapolateRight: "clamp",
-		}
-	);
-
-	const animatedLogoWatermark: CSSProperties = {
-		...logoWatermark,
-		opacity: fadeInOpacity,
-	};
-
-	const animatedContainer: CSSProperties = {
-		backgroundColor: "#1e2e75",
-		opacity: fadeOutOpacity,
-	};
-
-	return (
-		<AbsoluteFill style={animatedContainer}>
-			<AbsoluteFill style={logoWatermarkContainer}>
-				<Img
-					src={staticFile("remotion/logo-watermark.svg")}
-					style={animatedLogoWatermark}
-				/>
-			</AbsoluteFill>
-		</AbsoluteFill>
-	);
+  return (
+    <AbsoluteFill style={animatedContainer}>
+      <AbsoluteFill style={logoWatermarkContainer}>
+        <Img
+          src={staticFile("remotion/logo-watermark-the-end.svg")}
+          style={animatedLogoWatermark}
+        />
+      </AbsoluteFill>
+    </AbsoluteFill>
+  );
 };
 
 export default TheEndSegment;
--- src/features/edit-story/video-player/segments/portrait/the-end.tsx
+++ src/features/edit-story/video-player/segments/portrait/the-end.tsx
@@ -1,64 +1,49 @@
 import React, { CSSProperties } from "react";
 import {
-	AbsoluteFill,
-	staticFile,
-	Img,
-	useCurrentFrame,
-	interpolate,
-	useVideoConfig,
+  AbsoluteFill,
+  staticFile,
+  Img,
+  useCurrentFrame,
+  interpolate,
 } from "remotion";
-import { VIDEO_FPS, PREMOUNT_FRAMES } from "../../constants";
+import { VIDEO_FPS } from "../../constants";
 
 const logoWatermarkContainer: CSSProperties = {
-	justifyContent: "center",
-	alignItems: "center",
+  justifyContent: "center",
+  alignItems: "center",
 };
+
 const logoWatermark: CSSProperties = {
-	width: 300,
+  width: 400,
+};
+
+const animatedContainer: CSSProperties = {
+  backgroundColor: "#1e2e75",
 };
 
 const TheEndSegment = () => {
-	const frame = useCurrentFrame();
-	const { durationInFrames } = useVideoConfig();
+  const frame = useCurrentFrame();
 
-	const fadeInOpacity = interpolate(frame, [0, VIDEO_FPS], [0, 1], {
-		extrapolateLeft: "clamp",
-		extrapolateRight: "clamp",
-	});
+  const fadeInOpacity = interpolate(frame, [0, VIDEO_FPS], [0, 1], {
+    extrapolateLeft: "clamp",
+    extrapolateRight: "clamp",
+  });
 
-	const fadeOutOpacity = interpolate(
-		frame,
-		[
-			durationInFrames - PREMOUNT_FRAMES - VIDEO_FPS,
-			durationInFrames - PREMOUNT_FRAMES - 10, // a bit before ending video
-		],
-		[1, 0],
-		{
-			extrapolateLeft: "clamp",
-			extrapolateRight: "clamp",
-		}
-	);
-
-	const animatedLogoWatermark: CSSProperties = {
-		...logoWatermark,
-		opacity: fadeInOpacity,
-	};
-
-	const animatedContainer: CSSProperties = {
-		backgroundColor: "#1e2e75",
-		opacity: fadeOutOpacity,
-	};
-
-	return (
-		<AbsoluteFill style={animatedContainer}>
-			<AbsoluteFill style={logoWatermarkContainer}>
-				<Img
-					src={staticFile("remotion/logo-watermark.svg")}
-					style={animatedLogoWatermark}
-				/>
-			</AbsoluteFill>
-		</AbsoluteFill>
-	);
+  const animatedLogoWatermark: CSSProperties = {
+    ...logoWatermark,
+    opacity: fadeInOpacity,
+  };
+  
+  return (
+    <AbsoluteFill style={animatedContainer}>
+      <AbsoluteFill style={logoWatermarkContainer}>
+        <Img
+          src={staticFile("remotion/logo-watermark-the-end.svg")}
+          style={animatedLogoWatermark}
+        />
+      </AbsoluteFill>
+    </AbsoluteFill>
+  );
 };
 
 export default TheEndSegment;
