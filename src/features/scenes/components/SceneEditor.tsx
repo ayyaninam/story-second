24d8bb49c64681b9c87185253b13f2ae4ef6778e
5war00p <maddisaiswaroop@gmail.com>
2024-02-23 17:37:02 +0530

    perf: editor container (reusable code)

--- src/features/scenes/components/SceneEditor.tsx
+++ src/features/scenes/components/SceneEditor.tsx
@@ -1,139 +1,50 @@
+import { StoryboardViewType } from "@/utils/enums";
 import {
-	AspectRatios,
-	SegmentModifications,
-	StoryboardViewType,
-} from "@/utils/enums";
-import { ChevronDown, LayoutList, SparkleIcon } from "lucide-react";
+	ChevronDown,
+	Film,
+	HelpCircle,
+	LayoutList,
+	SparkleIcon,
+	ScrollText,
+} from "lucide-react";
+import { ModeToggle } from "@/features/edit-story/components/mode-toggle";
+import { Badge } from "@/components/ui/badge";
 import { useImmerReducer } from "use-immer";
 import editStoryReducer, {
 	EditStoryAction,
 	EditStoryDraft,
-	Scene,
-	Segment,
 	TextStatus,
 } from "../reducers/edit-reducer";
-import { GenerateStoryDiff, WebstoryToStoryDraft } from "../utils/storydraft";
+import { WebstoryToStoryDraft } from "../utils/storydraft";
 import { mainSchema } from "@/api/schema";
-import React, { useEffect, useRef, useState } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import api from "@/api";
-import EditSegmentModal from "./EditSegmentModal";
-import { SegmentModificationData } from "@/types";
-import { QueryKeys } from "@/lib/queryKeys";
-import { useRouter } from "next/router";
-import { VideoPlayerHandler } from "@/features/edit-story/components/video-player";
-// import ScriptEditorView from "./ScriptEditorView";
-// import StoryboardView from "./StoryboardViewTypesComponent";
-import StoryboardView from "./StoryboardView";
+import React from "react";
 import Editor from "./Editor";
 import { cn } from "@/utils";
 import Format from "@/utils/format";
 import StoryboardViewTypes from "./StoryboardViewTypesComponent";
 import AutosizeInput from "react-input-autosize";
 
-const MAX_SUMMARY_LENGTH = 251;
-
 export default function VideoEditorStoryboard({
 	WebstoryData,
-	ImageRatio,
-	isError,
-	isLoading,
 }: {
-	ImageRatio: {
-		width: number;
-		height: number;
-		ratio: number;
-		enumValue: AspectRatios;
-	};
 	WebstoryData?: mainSchema["ReturnVideoStoryDTO"];
-	isError?: boolean;
-	isLoading?: boolean;
 }) {
-	const router = useRouter();
-	const videoPlayerRef = useRef<VideoPlayerHandler | null>(null);
-	const queryClient = useQueryClient();
-	const [showFullDescription, setShowFullDescription] = useState(false);
-	const [isPlaying, setIsPlaying] = useState<boolean | undefined>();
-	const [seekedFrame, setSeekedFrame] = useState<number | undefined>();
-
-	const [editSegmentsModalState, setEditSegmentsModalState] = useState<{
-		open?: boolean;
-		scene?: Scene;
-		sceneId?: number;
-		dispatch?: React.Dispatch<EditStoryAction>;
-		story?: EditStoryDraft;
-	}>();
-
-	const [previousStory, setPreviousStory] = useState<EditStoryDraft>(
-		WebstoryToStoryDraft(WebstoryData!)
-	);
 	const [story, dispatch] = useImmerReducer<EditStoryDraft, EditStoryAction>(
 		editStoryReducer,
 		WebstoryToStoryDraft(WebstoryData!)
 	);
 
-	useEffect(() => {
-		dispatch({ type: "reset", draft: WebstoryToStoryDraft(WebstoryData!) });
-	}, [WebstoryData]);
-
-	const [selectedSegment, setSelectedSegment] = useState<Segment | null>(null);
-
-	const diff = GenerateStoryDiff(previousStory, story);
-
-	const EditSegment = useMutation({
-		mutationFn: api.video.editSegment,
-	});
-
-	const handleSubmitEditSegments = async () => {
-		const diff = GenerateStoryDiff(previousStory, story);
-		const edits: SegmentModificationData[] = diff.edits.map((segment) => ({
-			details: { Ind: segment.id, Text: segment.textContent },
-			operation: SegmentModifications.Edit,
-		}));
-		const additions: SegmentModificationData[] = diff.additions.map(
-			(segment) => ({
-				details: {
-					Ind: segment.id,
-					segments: [{ Text: segment.textContent, SceneId: segment.sceneId }],
-				},
-				operation: SegmentModifications.Add,
-			})
-		);
-		const deletions: SegmentModificationData[] = diff.subtractions.map(
-			(segment) => ({
-				details: {
-					Ind: segment.id,
-				},
-				operation: SegmentModifications.Delete,
-			})
-		);
-
-		const editedResponse = await EditSegment.mutateAsync({
-			story_id: WebstoryData?.id as string,
-			story_type: WebstoryData?.storyType,
-			edits: [...edits, ...additions, ...deletions],
-		});
-		queryClient.invalidateQueries({ queryKey: [QueryKeys.STORY] });
-
-		const newStory = await api.video.get(
-			WebstoryData?.topLevelCategory!,
-			WebstoryData?.slug!,
-			WebstoryData?.storyType!
-		);
-
-		setPreviousStory(WebstoryToStoryDraft(newStory));
-		dispatch({ type: "reset", draft: WebstoryToStoryDraft(newStory) });
-	};
-
-	useEffect(() => {
-		if (selectedSegment) {
-			console.log();
-			videoPlayerRef.current?.seekToSegment(selectedSegment);
-		}
-	}, [selectedSegment]);
-
 	return (
-		<>
+		<div className="relative z-0 w-full items-center justify-center flex flex-col h-full px-2 lg:px-5 py-2">
+			<div className="flex justify-center mt-auto">
+				<Badge
+					variant="outline"
+					className={`bg-primary-foreground font-normal text-sm`}
+				>
+					<ScrollText className="stroke-purple-600 mr-1 h-4 w-4" />
+					Generate & Edit Your Scenes
+				</Badge>
+			</div>
 			<div
 				className="w-[80%] m-auto"
 				style={{
@@ -147,7 +58,7 @@ export default function VideoEditorStoryboard({
 				<div className="w-full flex items-center justify-between gap-1 p-1 rounded-tl-lg rounded-tr-lg bg-primary-foreground font-normal text-xs border border-purple-500 bg-purple-100 text-purple-900">
 					<div className="flex items-center gap-1">
 						<LayoutList className="stroke-purple-600 mr-1 h-4 w-4" />
-						<p>Storyboard View</p>
+						<p>Scene Editor View</p>
 						<StoryboardViewTypes type={StoryboardViewType.Outline} />
 					</div>
 					<div className="flex gap-1 items-center">
@@ -269,41 +180,31 @@ export default function VideoEditorStoryboard({
 				</div>
 			</div>
 
-			{/* <StoryboardView
-				refs={refs}
-				dispatch={dispatch}
-				getSegmentStatus={getSegmentStatus}
-				handleEnter={handleEnter}
-				handleInput={handleInput}
-				setEditSegmentsModalState={setEditSegmentsModalState}
-				setPreviousStory={setPreviousStory}
-				story={story}
-				WebstoryData={WebstoryData}
-			/> */}
-			{/* <ScriptEditorView WebstoryData={WebstoryData} /> */}
+			<div className="flex justify-center mb-auto">
+				<Badge
+					variant="outline"
+					className={`bg-primary-foreground font-normal text-sm`}
+				>
+					<Film className="stroke-purple-600 mr-1 h-4 w-4" />
+					View & Share The Final Cut
+				</Badge>
+			</div>
+			<div className="absolute bottom-4 right-4 flex flex-col gap-y-3">
+				<span
+					className="rounded-full w-8 h-8 bg-popover p-1.5 flex items-center justify-center"
+					style={{ boxShadow: "0px 3px 6px 0px rgba(0, 0, 0, 0.13)" }}
+				>
+					<ModeToggle />
+				</span>
 
-			{editSegmentsModalState?.scene !== undefined &&
-				editSegmentsModalState?.sceneId !== undefined && (
-					<EditSegmentModal
-						open={
-							editSegmentsModalState?.open &&
-							editSegmentsModalState.scene !== undefined &&
-							editSegmentsModalState.sceneId !== undefined
-						}
-						onClose={() => setEditSegmentsModalState({})}
-						scene={editSegmentsModalState?.scene!}
-						sceneId={editSegmentsModalState?.sceneId}
-						dispatch={dispatch}
-						story={story}
-						onSceneEdit={(scene, index) => {
-							dispatch({
-								type: "edit_scene",
-								scene: scene,
-								index: index,
-							});
-						}}
-					/>
-				)}
-		</>
+				<span
+					className="rounded-full w-8 h-8 bg-popover p-1.5"
+					style={{ boxShadow: "0px 3px 6px 0px rgba(0, 0, 0, 0.13)" }}
+				>
+					<HelpCircle className="h-[18.286px] w-[18.286px] flex-shrink-0 stroke-slate-400" />
+				</span>
+			</div>
+			<div className="absolute w-[1px] h-screen bg-purple-300 z-[-1] mix-blend-multiply" />
+		</div>
 	);
 }
