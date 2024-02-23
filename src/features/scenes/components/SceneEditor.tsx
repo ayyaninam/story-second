44a33f4f2ae344b8c65555b33cc8f77db7723547
5war00p <maddisaiswaroop@gmail.com>
2024-02-23 23:26:46 +0530

    fix: scroll inside

--- src/features/scenes/components/SceneEditor.tsx
+++ src/features/scenes/components/SceneEditor.tsx
@@ -16,7 +16,6 @@ import {
 	Select,
 	SelectContent,
 	SelectItem,
-	SelectLabel,
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
@@ -80,6 +79,22 @@ const Loader = ({ percentage }: { percentage: number }) => {
 	);
 };
 
+const images = [
+	"https://s3-alpha-sig.figma.com/img/df90/21a4/e692a49276e76580e568857e2e98ecfd?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M8YPtAuPLlNk0hxZEUVte3QslxvEXaB4oCE~CKGSTELDpn9~0yElSFzbxR2o6kyT4IGx3HeKt0-Aa-YAOfCha3mBUW5I9pyvLCEU8R-jCq~R8TkgMZHW-dRxxahRuPS3ui1wNYvzWLPTE7X6PscEcV~FB5eP9nPWyMdvCgjDhrfw8Iwo-T64KzbZpIu8PFYUGeEBgYGuSmLOK-kOTbyDOopch8H2JaaId5E-NEna2gbXVlIr-wjKL0Kh34~1mWYJnAumIeIVkRPTPcoWnKduksrd3mzCHO6d78WQxar~6JTNveiz8Enws4d1PBgiRZ65jqWcKNNDnzsekL8SH9M~3Q__",
+	"https://s3-alpha-sig.figma.com/img/4b74/a308/440aa6436990ba59544eec4ffcdc563e?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NQuNSbFJk~IubBDht3JCZXWMewISqVltZtHPQN~XOIoMhBzh8Syk7wfE8XAUEEjwrW-3qYgN~yWut8QsQzuVkqg5d01Y-TWO2sIAXRbIfgDFex--Ev~ZgkPxDGxciPhy16O4PehAB-g4eq9j~Qq9fPfLKFJuqj9tTVO-GzOqROI~cGrduXj6O6BrwkFWRzbVHBzOzzzXbWaXgpTb83SOa1Mw8xIx8ij7foDG8Gs4NSVw56HeJ6NtTasw15DSL3N16NWD6DNfox~X1q1O4Z0Ty235gmjYh9FdsNjoKCX6qPcoCjpuxqiKIoCJEl~Kmr9~p7ypcu2HTlQ0eNmYuBHt8Q__",
+	"https://s3-alpha-sig.figma.com/img/37d6/3067/52049c46979d73390a22ddd36c091858?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jQxcQDduYjzAFCBraADR4Hh6LB0FpMIAYdzRhd0CqoBdC6TC6tdXP0~8mx7RRUGY7qeHGuc4p3QNqvQnm4OIhh8lnx6sVBIo7KeZxP89xIBTmbHVriNKyNQyucBBYt9aM72oNJ30MTTywI7j8RUnH-XmNRJKsPWrm3HhVdKRV6Iqb2DxWs5cVDHUpnIApxn16dG3KZgaXGchTF8dinm-Y5P-W17Gu218AeMrz9uzlNTa28iM38vBxKrm8O74OKGroY6XdhozST17dh6ouknWEI0qmmvxMcYacwZEjr-bJDWi1fZc6w0pVxYv8syc2RYHx51STgMiqWzbmfxTRasJzw__",
+	"https://s3-alpha-sig.figma.com/img/f693/d943/4a89adb3a043999da9fd93a00f46d036?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pHNVfsEyAMgYra620NFC5qLHp67xEp0lXuXdP3aDPJCkNECPqN51er1-J4f2imxeyu4Lf1lBbHoj3oQtxjpe2Xzv6Bq6mKt5Gfz16vDvG5LelsCkOelLaJJUO45i2PvkYbTZO7~-8LovqRDEan36E6x-JZxS4zmwcFIbVcXcc87ciNM6EoDdiG6UEhOLXn0W206d5YCO5TJwoRkrcgnINdwMNnAJs26zkwlf8GCIPVNZBRmI21DEwjg2N2rpWdcGgSmVUgocHekhxp8PO-HYESUwfaMxKhLlUMQxuGx9eqXxXtJlf-QaswTrZGu3FWXiyxbSMmtOm46ko9BL0WMLkA__",
+	"https://s3-alpha-sig.figma.com/img/db3e/2276/a5d1b15a94d2b0e6350dee3a7c20ff16?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cVCnfj7qxDhPX9xAUXksB7-dC8-NDeZL30EKw5rGeYkYJ-CnF4dMrGflFfnyVUgfZ0z7O5s7YuVEs-Wy2lGkwSXFrQg8UYCo99kf3beKvVJ8~gfq0jH3fsLv~64rBVFVC0pGEuibP3ZTA2uCyrX6Fws2ry5JsEXm0iTlmInyNqz2n~l6uqMr7wXpOvX-8PQrVJz~cLeWFz8qGZjOBR60UMaK7VlKeoi1QScNc~lHEOjwFKWPSM6w0DCpbjVbifr-FpbzOmhuZBgctGgQFCn3hCwQ1-LUDycccnbuS3sKf8G2113cP3QmkeqfMul0scskop482kiKNEXGTTtgs7g3CA__",
+	"https://s3-alpha-sig.figma.com/img/97c3/38ab/3840b8e82a3b15f65887698279769256?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BMYVki7bunQUrg~NoRX5~o9huPumsKjE1pYA1L6tHbvSdsfXdqTAp4zzp0IN8SphEnSIln2ZvlvYtjpf4N5STKBfgPVq4b6KWvjT8weDYM3kLi~bX7OLFn2N~-DRd5KFNfcbMAcMbZS4PCF8WVuXywLnf-bRbg7bMpyRYXkw7EluzZKkFozfutSLoqZxDKZsapMeuNb1TTPdD3NrmzHGeuxo5CbGz0YeBOJYInSe0mxnZJkjsbPS7WWdFKlHwxCeJiG74fN5wgG5hhfOUMW~exe79CkosefWuuHoBjYpJ0XIiFrq0pGHG8T0Zo0yzEsLRBJ5ogWLO8soekPenP6RMg__",
+	"https://s3-alpha-sig.figma.com/img/a4f0/438b/9fd5442c1e9eb8d094a4188d4adc9fea?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jsvJq0~YWdDwXOImQPvDFl~rPY3nmOF1ToG8re6IhR7IXk9j9BjeYIayiicYEdyz41K~tP2PJYpA0hv~fTnODHWtvC1XoWCP6CC-3eJyQbtDmVXlqrBRgHXAP1QesdP02ng2k-5Zru7fEXUsEcLwgRCPmUVsTA1RAGxJ~QKqaJHRXQiouxitBl2RS8PCcHsluIwxppCJz40x4q8pWIHMX4Z925meQhEfsamVR4ldA6bDMxAVZlz0gV06Fm5y60zeCoeEdvHqvQpwWHUQf4HipSm9BWJE6hKRIH4NmxsZ~6qEScz-iLtyAP05Kbl7ep78Bqi8vy8tFA0ema5sRsq90w__",
+	"https://s3-alpha-sig.figma.com/img/4b74/a308/440aa6436990ba59544eec4ffcdc563e?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NQuNSbFJk~IubBDht3JCZXWMewISqVltZtHPQN~XOIoMhBzh8Syk7wfE8XAUEEjwrW-3qYgN~yWut8QsQzuVkqg5d01Y-TWO2sIAXRbIfgDFex--Ev~ZgkPxDGxciPhy16O4PehAB-g4eq9j~Qq9fPfLKFJuqj9tTVO-GzOqROI~cGrduXj6O6BrwkFWRzbVHBzOzzzXbWaXgpTb83SOa1Mw8xIx8ij7foDG8Gs4NSVw56HeJ6NtTasw15DSL3N16NWD6DNfox~X1q1O4Z0Ty235gmjYh9FdsNjoKCX6qPcoCjpuxqiKIoCJEl~Kmr9~p7ypcu2HTlQ0eNmYuBHt8Q__",
+	"https://s3-alpha-sig.figma.com/img/37d6/3067/52049c46979d73390a22ddd36c091858?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jQxcQDduYjzAFCBraADR4Hh6LB0FpMIAYdzRhd0CqoBdC6TC6tdXP0~8mx7RRUGY7qeHGuc4p3QNqvQnm4OIhh8lnx6sVBIo7KeZxP89xIBTmbHVriNKyNQyucBBYt9aM72oNJ30MTTywI7j8RUnH-XmNRJKsPWrm3HhVdKRV6Iqb2DxWs5cVDHUpnIApxn16dG3KZgaXGchTF8dinm-Y5P-W17Gu218AeMrz9uzlNTa28iM38vBxKrm8O74OKGroY6XdhozST17dh6ouknWEI0qmmvxMcYacwZEjr-bJDWi1fZc6w0pVxYv8syc2RYHx51STgMiqWzbmfxTRasJzw__",
+	"https://s3-alpha-sig.figma.com/img/f693/d943/4a89adb3a043999da9fd93a00f46d036?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pHNVfsEyAMgYra620NFC5qLHp67xEp0lXuXdP3aDPJCkNECPqN51er1-J4f2imxeyu4Lf1lBbHoj3oQtxjpe2Xzv6Bq6mKt5Gfz16vDvG5LelsCkOelLaJJUO45i2PvkYbTZO7~-8LovqRDEan36E6x-JZxS4zmwcFIbVcXcc87ciNM6EoDdiG6UEhOLXn0W206d5YCO5TJwoRkrcgnINdwMNnAJs26zkwlf8GCIPVNZBRmI21DEwjg2N2rpWdcGgSmVUgocHekhxp8PO-HYESUwfaMxKhLlUMQxuGx9eqXxXtJlf-QaswTrZGu3FWXiyxbSMmtOm46ko9BL0WMLkA__",
+	"https://s3-alpha-sig.figma.com/img/db3e/2276/a5d1b15a94d2b0e6350dee3a7c20ff16?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cVCnfj7qxDhPX9xAUXksB7-dC8-NDeZL30EKw5rGeYkYJ-CnF4dMrGflFfnyVUgfZ0z7O5s7YuVEs-Wy2lGkwSXFrQg8UYCo99kf3beKvVJ8~gfq0jH3fsLv~64rBVFVC0pGEuibP3ZTA2uCyrX6Fws2ry5JsEXm0iTlmInyNqz2n~l6uqMr7wXpOvX-8PQrVJz~cLeWFz8qGZjOBR60UMaK7VlKeoi1QScNc~lHEOjwFKWPSM6w0DCpbjVbifr-FpbzOmhuZBgctGgQFCn3hCwQ1-LUDycccnbuS3sKf8G2113cP3QmkeqfMul0scskop482kiKNEXGTTtgs7g3CA__",
+	"https://s3-alpha-sig.figma.com/img/97c3/38ab/3840b8e82a3b15f65887698279769256?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BMYVki7bunQUrg~NoRX5~o9huPumsKjE1pYA1L6tHbvSdsfXdqTAp4zzp0IN8SphEnSIln2ZvlvYtjpf4N5STKBfgPVq4b6KWvjT8weDYM3kLi~bX7OLFn2N~-DRd5KFNfcbMAcMbZS4PCF8WVuXywLnf-bRbg7bMpyRYXkw7EluzZKkFozfutSLoqZxDKZsapMeuNb1TTPdD3NrmzHGeuxo5CbGz0YeBOJYInSe0mxnZJkjsbPS7WWdFKlHwxCeJiG74fN5wgG5hhfOUMW~exe79CkosefWuuHoBjYpJ0XIiFrq0pGHG8T0Zo0yzEsLRBJ5ogWLO8soekPenP6RMg__",
+	"https://s3-alpha-sig.figma.com/img/a4f0/438b/9fd5442c1e9eb8d094a4188d4adc9fea?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jsvJq0~YWdDwXOImQPvDFl~rPY3nmOF1ToG8re6IhR7IXk9j9BjeYIayiicYEdyz41K~tP2PJYpA0hv~fTnODHWtvC1XoWCP6CC-3eJyQbtDmVXlqrBRgHXAP1QesdP02ng2k-5Zru7fEXUsEcLwgRCPmUVsTA1RAGxJ~QKqaJHRXQiouxitBl2RS8PCcHsluIwxppCJz40x4q8pWIHMX4Z925meQhEfsamVR4ldA6bDMxAVZlz0gV06Fm5y60zeCoeEdvHqvQpwWHUQf4HipSm9BWJE6hKRIH4NmxsZ~6qEScz-iLtyAP05Kbl7ep78Bqi8vy8tFA0ema5sRsq90w__",
+];
+
 const SceneEditorView = ({
 	WebstoryData,
 	ImageRatio,
@@ -100,7 +115,7 @@ const SceneEditorView = ({
 
 	return (
 		<div
-			className="w-4/5 h-4/5 m-auto"
+			className="w-4/5 h-4/5 m-auto overflow-hidden"
 			style={{
 				borderRadius: "8px",
 				background: "#FEFEFF",
@@ -121,9 +136,9 @@ const SceneEditorView = ({
 					</a>
 				</p>
 			</div>
-			<div className="relative border-l border-slate-200 pl-4 flex flex-row m-6 gap-x-24 items-start">
+			<div className="relative h-full border-l border-slate-200 pl-4 flex flex-row m-6 gap-x-24 items-start">
 				<Loader percentage={20} />
-				<div className="relative items-start flex flex-col max-w-sm">
+				<div className="relative h-full items-start flex flex-col max-w-sm">
 					<div className="border-b pb-4 bg-[#FEFEFF]">
 						<p className="text-2xl font-bold max-w-sm -tracking-[-0.6px]">
 							{Format.Title(WebstoryData?.storyTitle)}
@@ -153,13 +168,10 @@ const SceneEditorView = ({
 							<p>by Anthony Deloso</p>
 						</div>
 					</div>
-					<div className="flex h-full flex-col justify-between">
+					<div className="flex flex-col h-screen justify-between overflow-y-scroll">
 						<div className="flex flex-col my-3 md:flex-row items-center w-full">
-							<div
-								className="w-full bg-background  rounded-bl-lg rounded-br-lg lg:rounded-br-lg lg:rounded-bl-lg flex flex-col lg:flex-row justify-stretch h-full"
-								// border-[1px]
-							>
-								<div className="flex w-full space-y-2 flex-col-reverse justify-between md:flex-col rounded-t-lg lg:rounded-bl-lg lg:rounded-tl-lg lg:rounded-tr-none lg:rounded-br-none">
+							<div className="w-full h-full bg-background  rounded-bl-lg rounded-br-lg lg:rounded-br-lg lg:rounded-bl-lg flex flex-col lg:flex-row justify-stretch">
+								<div className="flex w-full h-full space-y-2 flex-col-reverse justify-between md:flex-col rounded-t-lg lg:rounded-bl-lg lg:rounded-tl-lg lg:rounded-tr-none lg:rounded-br-none">
 									<Editor
 										Webstory={WebstoryData!}
 										dispatch={dispatch}
@@ -171,8 +183,22 @@ const SceneEditorView = ({
 													{story.scenes.map((scene, sceneIndex) => (
 														<div
 															key={sceneIndex}
-															className="flex group hover:border rounded-sm justify-between"
+															className="flex group hover:border rounded-sm items-center justify-between"
 														>
+															<div className="flex flex-shrink-0 flex-col -space-y-3 overflow-hidden mx-1 my-[18px] items-center justify-center">
+																{images.slice(0, 4).map((image, index) => (
+																	<div
+																		key={index}
+																		className="w-8 h-5 rounded-[1px]"
+																		style={{
+																			border: "0.2px solid rgba(0, 0, 0, 0.40)",
+																			background: `url(${image})`,
+																			backgroundSize: "cover",
+																			backgroundRepeat: "no-repeat",
+																		}}
+																	/>
+																))}
+															</div>
 															<div className="flex flex-wrap">
 																{scene.segments.map((segment, segmentIndex) => (
 																	<span
@@ -223,8 +249,12 @@ const SceneEditorView = ({
 																))}
 															</div>
 															<div className="hidden group-hover:flex gap-x-1 p-2">
-																<Settings2 className="w-4 h-4 stroke-slate-500" />
-																<MoreHorizontal className="w-4 h-4 stroke-slate-500" />
+																<span className="hover:bg-gray-100 cursor-pointer rounded-sm p-1">
+																	<Settings2 className="w-4 h-4 stroke-slate-500" />
+																</span>
+																<span className="hover:bg-gray-100 cursor-pointer rounded-sm p-1">
+																	<MoreHorizontal className="w-4 h-4 stroke-slate-500" />
+																</span>
 															</div>
 														</div>
 													))}
@@ -236,7 +266,7 @@ const SceneEditorView = ({
 							</div>
 						</div>
 
-						<div className="flex flex-col justify-end border-t">
+						<div className="mb-[5.25rem] flex flex-col justify-end border-t">
 							<span className="font-medium text-slate-400 mx-1.5 mt-1.5 mb-2.5 text-sm">
 								Use 25 credits to regenerate ·{" "}
 								<Link className="text-purple-600" href="#">
