feeb91b15fdbe1823d145481775f6597358e067a
Deep Mehta <deepme987@gmail.com>
2024-02-26 22:14:03 -0800

    misc: seo + redirect

--- src/pages/redirect-to-app.tsx
+++ src/pages/redirect-to-app.tsx
@@ -1,10 +1,9 @@
 import { GetServerSideProps } from "next";
-import { getSession } from "@auth0/nextjs-auth0";
 import Routes from "@/routes";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getSession(ctx.req, ctx.res);
-  const returnTo = session ? Routes.Library() : Routes.Explore();
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const returnTo = Routes.Explore();
   return {
     redirect: {
       destination: returnTo,
@@ -12,6 +11,18 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     },
   };
 };
+
+// Disabled: just redirect to /explore
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const session = await getSession(ctx.req, ctx.res);
+//   const returnTo = session ? Routes.Library() : Routes.Explore();
+//   return {
+//     redirect: {
+//       destination: returnTo,
+//       permanent: false,
+//     },
+//   };
+// };
 
 export default function Page() {
   return null;
