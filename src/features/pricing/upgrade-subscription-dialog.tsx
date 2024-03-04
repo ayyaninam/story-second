6775530893cb726c928e6c7fa924747c1806dac8
Oscar <odrr.99@gmail.com>
2024-03-03 20:29:25 -0800

    visual: more responsive checkout dialog

--- src/features/pricing/upgrade-subscription-dialog.tsx
+++ src/features/pricing/upgrade-subscription-dialog.tsx
@@ -1,36 +1,36 @@
 import React, { useState } from "react";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "@/components/ui/dialog";
 import PricingCards from "@/features/pricing/pricing-cards";
 
 interface UpgradeSubscriptionDialogProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const UpgradeSubscriptionDialog = ({
-  children,
+	children,
 }: UpgradeSubscriptionDialogProps) => {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="left-[50%] max-w-fit p-8 overflow-y-scroll lg:overflow-y-hidden max-h-[1200px] lg:max-h-none">
-        <DialogHeader>
-          <DialogTitle>Upgrade Subscription</DialogTitle>
-          <DialogDescription>
-            <PricingCards onClickFreePlan={() => setOpen(false)} />
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogTrigger asChild>{children}</DialogTrigger>
+			<DialogContent className="left-[50%] max-w-fit py-8 px-0 sm:p-8 overflow-y-scroll lg:overflow-y-hidden max-h-screen lg:max-h-none">
+				<DialogHeader>
+					<DialogTitle>Upgrade Subscription</DialogTitle>
+					<DialogDescription>
+						<PricingCards onClickFreePlan={() => setOpen(false)} />
+					</DialogDescription>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
 };
 
 export default UpgradeSubscriptionDialog;
