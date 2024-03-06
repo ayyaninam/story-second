6c407341769bb11978591ca5f68a329faa8b5d30
Deep Mehta <deepme987@gmail.com>
2024-03-05 21:50:26 -0800

    potential fix: cord

--- src/_middleware.js
+++
@@ -1,5 +0,0 @@
-// I don't think this works - tried using /feed, it didn't redirect
-
-// import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
-//
-// export default withMiddlewareAuthRequired();
