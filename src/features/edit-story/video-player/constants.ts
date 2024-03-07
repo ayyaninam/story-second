d6d83d869fc2c1cef7d691303e5f3b52f95afc9f
Oscar <odrr.99@gmail.com>
2024-03-06 16:10:14 -0800

    fix(remotion): this simple trick makes the remotion player get rids of the first audio issue

--- src/features/edit-story/video-player/constants.ts
+++ src/features/edit-story/video-player/constants.ts
@@ -115,6 +115,6 @@ export const bigZIndexTrick = 40000;
 // higher value means more video players existing one above other at the same time = more resources?
 // lower value increases the chance of flickering
 // todo: also this controls the transition duration, decouple this transition logic with the premount logic
-export const PREMOUNT_FRAMES = VIDEO_FPS;
+export const PREMOUNT_FRAMES = 1.5 * VIDEO_FPS;
 
 export const TO_THE_END_OF_VIDEO = 999999;
