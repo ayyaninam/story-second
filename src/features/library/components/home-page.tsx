4ce6330baac93967dd21e744ea5334344372a06a
Deep Mehta <deepme987@gmail.com>
2024-03-11 17:39:23 -0700

    library: show 7 widescreen

--- src/features/library/components/home-page.tsx
+++ src/features/library/components/home-page.tsx
@@ -33,7 +33,7 @@ function LibraryHomePage({
 			queryFn: () =>
 				api.library.getStories({
 					params: {
-						PageSize: 5,
+						PageSize: resolution === DisplayAspectRatios["1024x576"] ? 7 : 5,
 						CurrentPage: 1,
 						topLevelCategory: filterOptions.topLevelCategory,
 						isDescending: filterOptions.isDescending,
