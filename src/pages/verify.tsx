58936741a37624e51aac0c2233a957f26579a327
Deep Mehta <deepme987@gmail.com>
2024-05-10 02:02:10 +0530

    .

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -13,6 +13,7 @@ export const Verify = () => {
 		queryKey: [QueryKeys.USER],
 		queryFn: () => api.user.get(),
 		retry: false,
+		staleTime: 0,
 	});
 
 	const userIsLoggedId = Boolean(data);
@@ -28,26 +29,26 @@ export const Verify = () => {
 	};
 
 	useEffect(() => {
+		if (isLoading || !userIsLoggedId) return;
 		if (emailWasAlreadyVerified) {
 			redirectToFeed();
-		} else {
-			if (userIsLoggedId) {
-				try {
-					api.user.requestVerification().then(
-						() => {
-							setEmailIsVerified(true);
-							redirectToFeed();
-						},
-						() => {
-							setError(true);
-						}
-					);
-				} catch (e) {
+			return;
+		}
+
+		try {
+			api.user.requestVerification().then(
+				() => {
+					setEmailIsVerified(true);
+					redirectToFeed();
+				},
+				() => {
 					setError(true);
 				}
-			}
+			);
+		} catch (e) {
+			setError(true);
 		}
-	}, [router, emailWasAlreadyVerified]);
+	}, [router, emailWasAlreadyVerified, isLoading]);
 
 	let text = "Verifying email...";
 	if (error) {
@@ -66,5 +67,3 @@ export const Verify = () => {
 		<div className="flex flex-row justify-center items-center mt-8">{text}</div>
 	);
 };
-
-export default Verify;
